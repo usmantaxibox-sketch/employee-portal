d45e8165b386dbb3312c31f26d17203789fc1a0d
usmantaxibox-sketch <usmantaxibox@gmail.com>
2026-03-03 06:06:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -85,6 +85,10 @@ function logout() {
     window.location = "index.html";
 
 }
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
 
 
 
