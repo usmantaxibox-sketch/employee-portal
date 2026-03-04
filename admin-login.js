a14bb4a4763b490a0d48c0d7fa88bf5559308684
usmantaxibox-sketch <usmantaxibox@gmail.com>
2026-03-04 03:41:36 +0300

    Update admin-login.js

--- admin-login.js
+++ admin-login.js
@@ -5,7 +5,7 @@ document.getElementById("adminLoginForm").addEventListener("submit", function(e)
     const password = document.getElementById("adminPassword").value.trim();
     const errorMessage = document.getElementById("errorMessage");
 
-    const apiURL = "YOUR_WEB_APP_URL_HERE";
+    const apiURL = "https://script.google.com/macros/s/AKfycbzXcxoFj3ostP-KQW6bcZQwzGZSZ0Vwf-ZkM9AJADG20DhARVvy8DvMG0voe-bhtAbV/exec";
 
     fetch(apiURL)
         .then(response => response.json())
